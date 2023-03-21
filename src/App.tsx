4c9c3b3d1bfae3bd7157ca4c67b7779edf0caa7c
₳₳₳₳₳₳₳ <dwomeco2@github.fake>
2023-03-21 23:20:38 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>React UI Practice</h1>
+      <ul>
+        <li>
+          <a href="/layout">Layout</a>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import Layout from './routes/layout'
+
+import './index.css'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/layout",
+    element: <Layout />
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
