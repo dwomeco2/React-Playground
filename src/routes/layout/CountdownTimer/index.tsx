8d3d5ab92a28b681de0a512a49f2107ea416f448
₳₳₳₳₳₳₳ <dwomeco2@github.fake>
2023-03-23 11:01:13 +0800

    fix countdown timer animation

--- src/routes/layout/CountdownTimer/index.tsx
+++ src/routes/layout/CountdownTimer/index.tsx
@@ -12,7 +12,7 @@ function useCountDownTimer({ initTotalSeconds, timerInterval }: TimerProps["coun
     function reducer(state: TimerProps["state"], action: { type: string }): TimerProps["state"] {
         const { totalSeconds } = state;
         if ( action.type == 'tick' ) {
-            if ( totalSeconds >= 0 ) {
+            if ( totalSeconds > 0 ) {
                 return { totalSeconds: totalSeconds - 1 };
             }
             return { totalSeconds: 0 };
@@ -65,6 +65,64 @@ function randomWithRange(min: number, max: number) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+function mod(n: number, m: number) {
+    return ((n % m) + m) % m;
+}
+
+function FlippingCountDownComponent({ num, upperLimit, scale }: { num: number, upperLimit: number, scale: string }) {
+    // Add one here, when state is 1, we want to flipped to 1, not from 1 to 0
+    const current = (num + 1) % (upperLimit + 1);
+    const next = mod(current - 1, upperLimit + 1);
+
+    const contentNextTick = (element: Element) => {
+        element.innerHTML = ("" + mod(+element.innerHTML - 1, upperLimit + 1)).padStart(2, '0');
+    }
+
+    useEffect(() => {
+        const top_half = document.querySelector(`.${scale}.top-half`)!;
+        const bottom_half = document.querySelector(`.${scale}.bottom-half`)!;
+
+        const top_half_flip = document.querySelector(`.${scale}.top-half-flip`)!;
+        const bottom_half_flip = document.querySelector(`.${scale}.bottom-half-flip`)!;
+
+        const top_half_flip_animationstart_callback = () => { contentNextTick(top_half); };
+        const top_half_flip_animationend_callback = () => {
+            contentNextTick(top_half_flip);
+            top_half_flip.classList.remove('flip_animate');
+            bottom_half_flip.classList.add('flip_animate');
+        };
+        const bottom_half_flip_animmationend_callback = () => {
+            contentNextTick(bottom_half);
+            contentNextTick(bottom_half_flip);
+            bottom_half_flip.classList.remove('flip_animate');
+        };
+
+        top_half_flip.addEventListener('animationstart', top_half_flip_animationstart_callback);
+        top_half_flip.addEventListener('animationend', top_half_flip_animationend_callback);
+        bottom_half_flip.addEventListener('animationend', bottom_half_flip_animmationend_callback);
+
+        return () => {
+            top_half_flip.removeEventListener('animationstart', top_half_flip_animationstart_callback);
+            top_half_flip.removeEventListener('animationend', top_half_flip_animationend_callback);
+            bottom_half_flip.removeEventListener('animationend', bottom_half_flip_animmationend_callback);
+        }
+    }, []);
+
+    useEffect(() => {
+        const top_half_flip = document.querySelector(`.${scale}.top-half-flip`)!;
+        top_half_flip.classList.add('flip_animate');
+    }, [current]);
+
+    return (
+        <div className="flip_card">
+            <div className={`top-half ${scale}`}> {current}</div>
+            <div className={`bottom-half ${scale}`}> {current}</div>
+            <div className={`top-half-flip ${scale}`}> {current}</div>
+            <div className={`bottom-half-flip ${scale}`}> {next}</div>
+        </div>
+    )
+}
+
 export default function CountdownTimer() {
     const initTotalSeconds = randomWithRange(1, 8639999);
 
@@ -77,20 +135,20 @@ export default function CountdownTimer() {
             <div className="mb-16">WE'RE LAUNCHING SOON</div>
             <div className="flex justify-center gap-4">
                 <div className="flex flex-col">
-                    <span className="text-xs">{days}</span>
-                    <div className="font-bold">Days</div>
+                    <FlippingCountDownComponent num={days} upperLimit={99} scale={"countdown_days"} />
+                    <div className="font-bold mt-6">Days</div>
                 </div>
                 <div className="flex flex-col">
-                    <span className="text-xs">{hours}</span>
-                    <div className="font-bold">Hours</div>
+                    <FlippingCountDownComponent num={hours} upperLimit={23} scale={"countdown_hours"} />
+                    <div className="font-bold mt-6">Hours</div>
                 </div>
                 <div className="flex flex-col">
-                    <span className="text-xs">{minutes}</span>
-                    <div className="font-bold">Minutes</div>
+                    <FlippingCountDownComponent num={minutes} upperLimit={59} scale={"countdown_minutes"} />
+                    <div className="font-bold mt-6">Minutes</div>
                 </div>
                 <div className="flex flex-col">
-                    <span className="text-xs">{seconds}</span>
-                    <div className="font-bold">Seconds</div>
+                    <FlippingCountDownComponent num={seconds} upperLimit={59} scale={"countdown_second"} />
+                    <div className="font-bold mt-6">Seconds</div>
                 </div>
             </div>
             <div>social icons</div>
