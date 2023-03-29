92f10e6ada1ad5366a1ea0132a26b04821eba808
₳₳₳₳₳₳₳ <kennylamwk@gmail.com>
2023-03-29 15:55:51 +0800

    add portal: githubcorner

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import { createPortal } from 'react-dom'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import Layout from './routes/layout'
+import GithubCorner from './component/GithubCorner/index'
 
 import './index.css'
 
@@ -18,8 +20,11 @@ const router = createBrowserRouter([
   }
 ])
 
+const githubCornerDiv = document.getElementById('github-corner-div') as HTMLElement
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <>
     <RouterProvider router={router} />
+    {createPortal(<GithubCorner />, githubCornerDiv)}
   </>
 )
