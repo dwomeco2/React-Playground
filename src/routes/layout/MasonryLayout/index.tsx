c68e3baa20d1878f22fb395e06af38466b0cdd51
₳₳₳₳₳₳₳ <kennylamwk@gmail.com>
2023-03-30 12:41:39 +0800

    add intersection observer & infinite scroll

--- src/routes/layout/MasonryLayout/index.tsx
+++ src/routes/layout/MasonryLayout/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, createRef } from 'react'
+import { useState, useEffect, createRef, Dispatch, SetStateAction } from 'react'
 import imagesjson from './images.json'
 
 type imageType = {
@@ -10,26 +10,11 @@ type imageType = {
 
 type imageBoundingRect = { ref: React.RefObject<HTMLDivElement>; br: DOMRect }
 
-export default function MasonryLayout() {
-  const [images, setImages] = useState<imageType[]>(
-    imagesjson.images.map(item => {
-      return { ...item, ref: createRef<HTMLDivElement>() }
-    })
-  )
+type AnimatedLayoutHookType = [imageType[], Dispatch<SetStateAction<imageType[]>>]
+
+function useAnimatedImage(imagesProp: imageType[] | null): AnimatedLayoutHookType {
+  const [images, setImages] = useState<imageType[]>(imagesProp || [])
   const [domRects, setDomRects] = useState<imageBoundingRect[]>()
-
-  function randomizeOnclick() {
-    const newImages = [...images]
-
-    for (let n = 0; n < newImages.length - 1; n++) {
-      const k = n + Math.floor(Math.random() * (newImages.length - n))
-
-      const temp = newImages[k]
-      newImages[k] = newImages[n]
-      newImages[n] = temp
-    }
-    setImages(newImages)
-  }
 
   useEffect(() => {
     const dr = images.map(item => {
@@ -44,7 +29,7 @@ export default function MasonryLayout() {
           const deltaY = oldItem.br.top - newItem.br.top
 
           if (deltaX !== 0 || deltaY !== 0) {
-            console.log(deltaX, deltaY)
+            // console.log(deltaX, deltaY)
             domNode.style.transform = `translate(${deltaX}px, ${deltaY}px)`
             domNode.style.transition = 'transform 0s'
             requestAnimationFrame(() => {
@@ -64,30 +49,107 @@ export default function MasonryLayout() {
     setDomRects(dr)
   }, [images])
 
+  return [images, setImages]
+}
+
+export default function MasonryLayout() {
+  const [images, setImages] = useAnimatedImage(
+    imagesjson.images.map(item => {
+      return { ...item, ref: createRef<HTMLDivElement>() }
+    })
+  )
+  const [isLoading, setIsLoading] = useState(false)
+
+  function addFiveImage() {
+    const maxId = Math.max(...images.map(item => item.id))
+    const result: any = []
+    for (let i = 0; i < 5; i++) {
+      result.push(addImage(maxId + i))
+    }
+
+    // simulate 2 seconds delay
+    setIsLoading(true)
+    setTimeout(() => {
+      setIsLoading(false)
+      setImages([...images, ...result])
+    }, 2000)
+  }
+
+  useEffect(() => {
+    const scroller = document.querySelector('.scroller')
+    const obCallback = (entries: IntersectionObserverEntry[]) => {
+      if (isLoading) return
+      if (scroller == null || scroller.scrollHeight <= scroller.clientHeight) return
+      for (const e of entries) {
+        if (e.isIntersecting) {
+          // Scrolled to bottom
+          addFiveImage()
+        }
+      }
+    }
+    const ob = new IntersectionObserver(obCallback, {
+      root: scroller,
+      threshold: 1
+    })
+    ob?.observe(scroller?.lastElementChild as HTMLElement)
+    return () => {
+      ob?.unobserve(scroller?.lastElementChild as HTMLElement)
+    }
+  }, [images, isLoading])
+
   return (
     <div>
       <p>masonry in grid layout only implemented by firefox</p>
-      <div>
-        <button className="bg-slate-700 rounded-full py-2 px-8 my-2 text-white" onClick={randomizeOnclick}>
+      <div className="flex gap-x-4 justify-center my-6">
+        <button className="bg-slate-700 rounded-full py-2 px-8 text-white" onClick={() => setImages(randomize(images))}>
           Randomize
         </button>
+        <button className="bg-red-500 rounded-full py-2 px-8 text-white" onClick={addFiveImage}>
+          Add 5
+        </button>
       </div>
-      <div className="columns-3 gap-2">
-        {images.map(item => {
-          return (
-            <div key={item.id} ref={item.ref} className="flex justify-center items-center relative mb-2 hover:scale-110 hover:z-10 cursor-pointer">
-              <div className="absolute text-5xl font-bold text-white">{item.id}</div>
-              <img
-                className={`w-full aspect-video`}
-                style={{
-                  height: `${item.h}`,
-                  backgroundColor: `${item.bg}`
-                }}
-              />
-            </div>
-          )
-        })}
+      <div className="scroller max-h-[50rem] overflow-y-scroll bg-gray-400">
+        <div className="columns-3 gap-2 w-full rounded-md p-6">
+          {images.map(item => {
+            return (
+              <div key={item.id} ref={item.ref} className="flex justify-center items-center relative mb-2 hover:scale-110 hover:z-10 cursor-pointer">
+                <div className="absolute text-5xl font-bold text-white">{item.id}</div>
+                <img
+                  className={`w-full aspect-video`}
+                  style={{
+                    height: `${item.h}`,
+                    backgroundColor: `${item.bg}`
+                  }}
+                />
+              </div>
+            )
+          })}
+        </div>
+        {isLoading && <div> Loading </div>}
+        {/* element for IntersectionObserver */}
+        <div className="w-full h-2 mb-40"></div>
       </div>
     </div>
   )
+}
+
+function randomize(images: any[]) {
+  const newImages = [...images]
+
+  for (let n = 0; n < newImages.length - 1; n++) {
+    const k = n + Math.floor(Math.random() * (newImages.length - n))
+
+    const temp = newImages[k]
+    newImages[k] = newImages[n]
+    newImages[n] = temp
+  }
+
+  return newImages
+}
+
+function addImage(maxId: number) {
+  const randomHeight = `${Math.floor(Math.random() * (38 - 10 + 1)) + 10}rem`
+  const randomColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`
+
+  return { id: maxId + 1, h: randomHeight, bg: randomColor, ref: createRef<HTMLDivElement>() }
 }
