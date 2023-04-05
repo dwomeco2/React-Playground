4c096cea6951c73a937f12dad9e982c4496d7a8a
₳₳₳₳₳₳₳ <kennylamwk@gmail.com>
2023-04-05 09:55:20 +0800

    init

--- src/components/IssuesWithLibrary/index.tsx
+++ src/components/IssuesWithLibrary/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, Suspense } from 'react'
 
 export default function IssueWithLibrary() {
   const [issue, setIssue] = useState(0)
@@ -19,20 +19,24 @@ export default function IssueWithLibrary() {
       </div>
       <div className="w-full h-full">
         {issue == 1 && (
-          <iframe
-            src="https://codesandbox.io/embed/backgroundcolor-in-framer-motion-forked-fij3sd?fontsize=14&hidenavigation=1&theme=dark"
-            style={{ width: '100%', height: '100%', border: 0, borderRadius: '4px', overflow: 'hidden' }}
-            title="backgroundColor in Framer-motion"
-            sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
-          ></iframe>
+          <Suspense fallback={<div>Loading...</div>}>
+            <iframe
+              src="https://codesandbox.io/embed/backgroundcolor-in-framer-motion-forked-fij3sd?fontsize=14&hidenavigation=1&theme=dark"
+              style={{ width: '100%', height: '100%', border: 0, borderRadius: '4px', overflow: 'hidden' }}
+              title="backgroundColor in Framer-motion"
+              sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+            ></iframe>
+          </Suspense>
         )}
         {issue == 2 && (
-          <iframe
-            src="https://codesandbox.io/embed/dnd-kit-resortable-lots-of-render-issue-ftlh3c?fontsize=14&hidenavigation=1&theme=dark"
-            style={{ width: '100%', height: '100%', border: 0, borderRadius: '4px', overflow: 'hidden' }}
-            title="Dnd-kit Resortable lots of render issue"
-            sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
-          ></iframe>
+          <Suspense fallback={<div>Loading...</div>}>
+            <iframe
+              src="https://codesandbox.io/embed/dnd-kit-resortable-lots-of-render-issue-ftlh3c?fontsize=14&hidenavigation=1&theme=dark"
+              style={{ width: '100%', height: '100%', border: 0, borderRadius: '4px', overflow: 'hidden' }}
+              title="Dnd-kit Resortable lots of render issue"
+              sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+            ></iframe>
+          </Suspense>
         )}
       </div>
     </div>
