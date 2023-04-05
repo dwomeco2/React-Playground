9e09f25cd8c69afefdb0f36b38e97549d32c9c37
₳₳₳₳₳₳₳ <kennylamwk@gmail.com>
2023-04-05 09:40:35 +0800

    add todolist

--- src/components/TailwindUIFramework/Todolist/index.tsx
+++ src/components/TailwindUIFramework/Todolist/index.tsx
@@ -1,30 +1,80 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useCallback } from 'react'
 import { z } from 'zod'
 import { nanoid } from 'nanoid'
 import { CgSearch } from 'react-icons/cg'
-import { TbSelect } from 'react-icons/tb'
-import { MdExpandMore } from 'react-icons/md'
-import { DndContext, useSensors, useSensor, MouseSensor, TouchSensor, closestCenter, DragEndEvent } from '@dnd-kit/core'
-import { SortableContext, useSortable, verticalListSortingStrategy } from '@dnd-kit/sortable'
-import { CSS } from '@dnd-kit/utilities'
+import { FaTrash } from 'react-icons/fa'
+import { DndProvider, useDrag, useDrop } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
 
-const todoItemZod = z.object({ id: z.string(), text: z.string(), checked: z.boolean().default(false), hasContent: z.boolean().default(false) })
+const todoItemZod = z.object({ id: z.string(), text: z.string(), checked: z.boolean().default(false) })
 const todoZod = z.array(todoItemZod).default([])
 type todoListType = z.infer<typeof todoZod>
 
-const TodoItem = (props: any) => {
-  const { handleTodoChecked, ...todo } = props
-  const { attributes, listeners, transform, transition, setNodeRef } = useSortable({ id: todo.id })
-  const style = {
-    transform: CSS.Transform.toString(transform),
-    transition
-  }
+const dragItem = z.object({ id: z.string(), index: z.number() })
+type dragItemType = z.infer<typeof dragItem>
 
-  // Lots of rerendering...
-  console.log(todo)
+const TodoItem = (props: any) => {
+  const { handleTodoChecked, handleDrop, deleteTodoItem, ...todo } = props
+
+  const ref = useRef<HTMLDivElement>(null)
+
+  const [, drop] = useDrop({
+    accept: 'todo',
+    hover(item: dragItemType, monitor) {
+      if (!ref.current) {
+        return
+      }
+      const dragIndex = item.index
+      const hoverIndex = todo.index
+      // Don't replace items with themselves
+      if (dragIndex === hoverIndex) {
+        return
+      }
+
+      // Determine mouse position
+      const clientOffset = monitor.getClientOffset()
+      if (clientOffset == null) {
+        return
+      }
+
+      // Determine rectangle on screen
+      const hoverBoundingRect = ref.current?.getBoundingClientRect()
+      // Get vertical middle
+      const hoverMiddleY = (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2
+
+      // Get pixels to the top
+      const hoverClientY = clientOffset.y - hoverBoundingRect.top
+      // Only perform the move when the mouse has crossed half of the items height
+      // When dragging downwards, only move when the cursor is below 50%
+      // When dragging upwards, only move when the cursor is above 50%
+      // Dragging downwards
+      if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
+        return
+      }
+      // Dragging upwards
+      if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
+        return
+      }
+      // console.log(`drag: ${dragIndex}`, `hover: ${hoverIndex}`)
+      // Time to actually perform the action
+      handleDrop(dragIndex, hoverIndex)
+      // Note: we're mutating the monitor item here!
+      // Generally it's better to avoid mutations,
+      // but it's good here for the sake of performance
+      // to avoid expensive index searches.
+      item.index = hoverIndex
+    }
+  })
+
+  const [, drag] = useDrag({
+    type: 'todo',
+    item: { id: todo.id, index: props.index }
+  })
+
+  drag(drop(ref))
 
   return (
-    <div ref={setNodeRef} style={style} {...attributes} {...listeners}>
+    <div ref={ref}>
       <div className="flex items-center hover:bg-gray-800 rounded-md">
         <div className="flex grow ">
           <div className="flex items-center px-1">
@@ -38,13 +88,23 @@ const TodoItem = (props: any) => {
             />
           </div>
           <div className={`grow text-start py-2 px-1 text-gray-200 border-gray-300 ${todo.checked && 'line-through'} cursor-pointer`}>{todo.text}</div>
-          {todo.hasContent && (
-            <button type="button" onClick={() => {}} className="text-gray-200 text-lg py-2 px-1 hover:bg-gray-800 rounded-md">
-              <MdExpandMore />
-            </button>
-          )}
+          <button type="button" onClick={() => deleteTodoItem(todo.id)} className="text-gray-200 text-lg py-2 px-1 hover:bg-gray-800 rounded-md">
+            <FaTrash />
+          </button>
         </div>
       </div>
+    </div>
+  )
+}
+
+const TodoContainer = ({ children, className }: { children: JSX.Element[] | JSX.Element; className: string }) => {
+  const [, drop] = useDrop(() => ({
+    accept: 'todo'
+  }))
+
+  return (
+    <div ref={drop} className={className}>
+      {children}
     </div>
   )
 }
@@ -52,9 +112,6 @@ const TodoItem = (props: any) => {
 const TodoList = () => {
   const [todos, setTodos] = useState<todoListType>([])
   const [inputValue, setInputValue] = useState('')
-  const [onSelect, setOnSelect] = useState(false)
-
-  const sensors = useSensors(useSensor(MouseSensor), useSensor(TouchSensor))
 
   const handleInputChange = (event: { target: { value: React.SetStateAction<string> } }) => {
     setInputValue(event.target.value)
@@ -69,29 +126,34 @@ const TodoList = () => {
   }
 
   const handleTodoChecked = (id: string, event: React.ChangeEvent<HTMLInputElement>) => {
-    const newTodos = [...todos]
-    let todoIndex = newTodos.findIndex(todo => todo.id === id)
-    newTodos[todoIndex] = todoItemZod.parse({ ...newTodos[todoIndex], checked: event.target.checked })
-    setTodos(newTodos)
+    setTodos(prev => {
+      const newTodos = [...prev]
+      let todoIndex = newTodos.findIndex(todo => todo.id === id)
+      newTodos[todoIndex] = todoItemZod.parse({ ...newTodos[todoIndex], checked: event.target.checked })
+      return newTodos
+    })
   }
 
-  // Function to update list on drop
-  const handleDrop = (event: DragEndEvent) => {
-    const { active, over } = event
-    if (over && active.id !== over.id) {
-      let updatedList = [...todos]
-      const activeIndex = updatedList.findIndex(todo => todo.id === active.id)
-      const overIndex = updatedList.findIndex(todo => todo.id === over.id)
+  const handleDrop = (dragIndex: number, hoverIndex: number) => {
+    setTodos(prev => {
+      let updatedList = [...prev]
 
-      const [reorderedItem] = updatedList.splice(activeIndex, 1)
-      updatedList.splice(overIndex, 0, reorderedItem)
+      const [dragItem] = updatedList.splice(dragIndex, 1)
+      updatedList.splice(hoverIndex, 0, dragItem)
 
-      setTodos(updatedList)
-    }
+      return updatedList
+    })
+  }
+
+  const deleteTodoItem = (id: string) => {
+    setTodos(prev => prev.filter(todo => todo.id !== id))
   }
 
   return (
     <div className="max-w-lg mx-auto text-gray-800 bg-gray-900 p-2 rounded-md">
+      <div>
+        <h1 className="text-gray-300 text-2xl font-bold mb-2">Todo List</h1>
+      </div>
       <form onSubmit={handleFormSubmit} className={`flex gap-2 outline-0 ${todos && todos[0] ? 'mb-4' : ''}`}>
         <div className="flex grow relative">
           <div className="text-gray-300 absolute-center-y left-2">
@@ -105,19 +167,14 @@ const TodoList = () => {
             className="flex-1 placeholder-gray-500 text-gray-200 bg-gray-600 rounded-md p-2 reset-input pl-8"
           />
         </div>
-        <button type="button" className={`${onSelect ? 'bg-green-500' : 'bg-orange-400'} text-white px-4 py-2 rounded-md`} onClick={() => setOnSelect(!onSelect)}>
-          <TbSelect />
-        </button>
       </form>
-      <DndContext sensors={sensors} collisionDetection={closestCenter} onDragEnd={handleDrop}>
-        <SortableContext strategy={verticalListSortingStrategy} items={todos}>
-          <div className="list-container max-h-[32rem]">
-            {todos.map(item => (
-              <TodoItem key={item.id} handleTodoChecked={handleTodoChecked} {...item} />
-            ))}
-          </div>
-        </SortableContext>
-      </DndContext>
+      <DndProvider backend={HTML5Backend}>
+        <TodoContainer className="list-container max-h-[32rem]">
+          {todos.map((item, index) => (
+            <TodoItem key={item.id} index={index} handleTodoChecked={handleTodoChecked} handleDrop={handleDrop} deleteTodoItem={deleteTodoItem} {...item} />
+          ))}
+        </TodoContainer>
+      </DndProvider>
     </div>
   )
 }
