1563013cf14f1ee5af86c23c512fc44cc9f2dc2c
₳₳₳₳₳₳₳ <kennylamwk@gmail.com>
2023-04-07 11:42:59 +0800

    rewrite

--- src/components/TailwindUIFramework/Game2048/index.tsx
+++ src/components/TailwindUIFramework/Game2048/index.tsx
@@ -2,26 +2,6 @@ import { useState, useEffect, useMemo, useReducer } from "react"
 import { nanoid } from "nanoid"
 import { z } from "zod"
 
-const useGlobalKeyDownEffect = <T extends unknown>(
-	keys: string[],
-	callbacks: ((deps: T[]) => void)[],
-	dependencies: T[]
-) => {
-	const keyDownCallback = (e: KeyboardEvent) => {
-		keys.forEach((key, index) => {
-			if (e.key == key) {
-				callbacks[index](dependencies)
-			}
-		})
-	}
-	useEffect(() => {
-		window.addEventListener("keydown", keyDownCallback)
-		return () => {
-			window.removeEventListener("keydown", keyDownCallback)
-		}
-	}, [dependencies])
-}
-
 const colors = [
 	"#392A1A",
 	"#473616",
@@ -36,100 +16,32 @@ const colors = [
 	"#EEC12E"
 ]
 
-type ActionsType = {
-	type: "batch"
-	payload: {
-		actions: ActionType[]
-	}
-}
-
-type ActionType = {
-	type: "move" | "merge"
-	payload: {
-		cellID: string
-		from?: {
-			row: number
-			col: number
-		}
-		to?: {
-			row: number
-			col: number
-		}
-		unmountCellID?: string
-	}
-}
-
-function reducer(state: cellsType, actions: ActionsType) {
-	console.log(actions)
-	if (actions.type === "batch") {
-		let newState = state
-		actions.payload.actions.forEach(action => {
-			let cell = newState.find(cell => cell.id === action.payload.cellID)
-			let unmountCell = newState.find(
-				cell => cell.id === action.payload.unmountCellID
-			)
-			if (cell) {
-				cell.prevCor = action.payload.from
-				cell.cor = action.payload.to!
-				if (action.type === "merge") {
-					cell.val *= 2
-				}
-			}
-			if (unmountCell && unmountCell.val === 0) {
-				unmountCell.unmount = true
-			}
-		})
-		newState = newState.filter(cell => !cell.unmount)
-
-		let seq = [...Array(16).keys()]
-		seq.forEach(item => {
-			let { row, col } = indexToRowCol(item)
-			let cell = newState.find(
-				cell => cell.cor.row === row && cell.cor.col === col
-			)
-			if (!cell) {
-				let newCell = { val: 0, cor: { row, col }, id: nanoid() }
-				newState.push(newCell as cellType)
-			}
-		})
-
-		let index
-		do {
-			index = Math.floor(Math.random() * 16)
-		} while (newState[index].val != 0)
-		newState[index].val = spawnVal()
-
-		// setCells(arr)
-
-		return newState
-	}
-	throw Error("Unknown action")
-}
-
 export default function Game2048() {
 	const [cells, dispatch] = useReducer(reducer, initializeGame())
-	const endState = useState(false)
+	const [isEnd, setEnd] = useState(false)
 
 	useGlobalKeyDownEffect(
 		["ArrowRight", "ArrowLeft", "ArrowUp", "ArrowDown"],
 		[
-			onArrowRightKeyDown(dispatch),
-			onArrowLeftKeyDown(dispatch),
-			onArrowUpKeyDown(dispatch),
-			onArrowDownKeyDown(dispatch)
+			onArrowKeyDown(rightSequence, dispatch),
+			onArrowKeyDown(leftSequence, dispatch),
+			onArrowKeyDown(upSequence, dispatch),
+			onArrowKeyDown(downSequence, dispatch)
 		],
-		[cells, endState]
+		[]
 	)
 
-	const isEnd = endState[0]
-
-	// let sortedCells = useMemo(
-	// 	() =>
-	// 		cells.sort(
-	// 			(a, b) => a.cor.row * 4 + a.cor.col - (b.cor.row * 4 + b.cor.col)
-	// 		),
-	// 	[cells]
-	// )
+	useEffect(() => {
+		if (cells.every(cell => cell.val !== 0)) {
+			let horizontal = [0, 1, 2, 3].map(i => leftSequence(i).map(indexToRowCol))
+			let vertical = [0, 1, 2, 3].map(i => upSequence(i).map(indexToRowCol))
+			const horizontalChanges = filterNoChange(horizontal, cells)
+			const verticalChanges = filterNoChange(vertical, cells)
+			if (horizontalChanges.length === 0 && verticalChanges.length === 0) {
+				setEnd(true)
+			}
+		}
+	}, [cells])
 
 	return (
 		<div>
@@ -157,24 +69,174 @@ export default function Game2048() {
 		</div>
 	)
 }
-const CordinateZod = z.object({
-	row: z.number().max(3),
-	col: z.number().max(3)
-})
 
-const cellZ = z.object({
-	id: z.string(),
-	val: z.number().default(0),
-	cor: CordinateZod,
-	prevCor: CordinateZod.optional(),
-	unmount: z.boolean().default(false)
-})
+type ActionType = {
+	type: "arrowkey"
+	payload: {
+		lines: {
+			row: number
+			col: number
+		}[][]
+	}
+}
 
-const cellsZ = z.array(cellZ)
-type cellType = z.infer<typeof cellZ>
-type cellsType = z.infer<typeof cellsZ>
+function reducer(state: CellsType, action: ActionType) {
+	let cells = state
+	if (action.type === "arrowkey") {
+		// preprocess
+		const result = filterNoChange(action.payload.lines, cells)
+			.map(l => {
+				let line = l.map(deepClone)
+				return moveOrMerge(line as CellsType)
+			})
+			.flat()
+
+		if (result.length === 0) return state
+
+		let newState = deepClone(state) as CellsType
+		result.forEach(({ merged, unmount }) => {
+			// merge
+			merged.forEach(m => {
+				let index = newState.findIndex(cell => cell.id === m.id)
+				if (index !== -1) {
+					newState[index] = m
+				} else {
+					newState.push(m)
+				}
+			})
+			// unmount
+			unmount.forEach(id => {
+				newState = newState.filter(cell => cell.id !== id)
+			})
+		})
+
+		// Arrow key with no changes
+		if (equalityCompare(state, newState)) {
+			console.log("no changes")
+			return state
+		}
+
+		// respwan
+		if (newState.findIndex(cell => cell.val === 0) !== -1) {
+			newState = spawn(newState)
+		}
+
+		return newState
+	}
+	throw Error("Unknown action")
+}
+
+type moveOrMergeReturnType = {
+	merged: CellsType
+	unmount: string[]
+}
+
+// Expecting all line move to the left
+const moveOrMerge = (arr: CellsType) => {
+	let unmount = [] as string[]
+	let i = 0
+	let j = 1
+	while (i < 3) {
+		if (j >= 4) {
+			i++
+			j = i + 1
+			continue
+		}
+		if (arr[i].val === arr[j].val && arr[i].val != 0) {
+			// merge
+			arr[j].prevCor = arr[j].cor
+			arr[j].cor = arr[i].cor
+			arr[j].val *= 2
+			unmount.push(arr[i].id)
+			arr[i] = arr[j]
+			arr[j] = { id: "tombstone", val: 0, cor: arr[j].prevCor! }
+			i++
+		} else if (arr[i].val === 0 && arr[j].val != 0) {
+			// move
+			arr[j].prevCor = arr[j].cor
+			arr[j].cor = arr[i].cor
+			unmount.push(arr[i].id)
+			arr[i] = arr[j]
+			arr[j] = { id: "tombstone", val: 0, cor: arr[j].prevCor! }
+		} else if (arr[i].val !== arr[j].val && arr[j].val != 0) {
+			i++
+			j = i + 1
+			continue
+		}
+		j++
+	}
+
+	// replace all tombstone with new one
+	arr.forEach(cell => {
+		if (cell.id === "tombstone") {
+			cell.id = nanoid()
+		}
+	})
+
+	unmount = unmount.filter(id => !arr.find(cell => cell.id === id))
+
+	return { merged: arr, unmount } as moveOrMergeReturnType
+}
+
+const indexToRowCol = (i: number) => ({ row: Math.floor(i / 4), col: i % 4 })
+
+const onArrowKeyDown = (
+	sequence: (i: number) => number[],
+	dispatch: React.Dispatch<ActionType>
+) => {
+	return () => {
+		let arr = [0, 1, 2, 3].map(i => sequence(i).map(indexToRowCol))
+
+		dispatch({ type: "arrowkey", payload: { lines: arr } })
+	}
+}
+
+const filterNoChange = (
+	lines: ActionType["payload"]["lines"],
+	cells: CellsType
+) => {
+	return lines
+		.map(l =>
+			l.map(
+				({ row, col }) =>
+					cells.find(cell => cell.cor.row === row && cell.cor.col === col)!
+			)
+		)
+		.filter(l => !l.every(cell => cell.val === 0))
+		.filter(l => {
+			let hasZero = !l.every(cell => cell.val !== 0)
+			let hasConsecutiveSameValue = false
+			for (let i = 0; i < l.length - 1; i++) {
+				if (l[i].val === l[i + 1].val) {
+					hasConsecutiveSameValue = true
+				}
+			}
+			return hasZero || hasConsecutiveSameValue
+		})
+}
+
+const leftSequence = (i: number) => [i * 4, i * 4 + 1, i * 4 + 2, i * 4 + 3]
+const rightSequence = (i: number) => leftSequence(i).reverse()
+const upSequence = (i: number) => [i, i + 4, i + 8, i + 12]
+const downSequence = (i: number) => upSequence(i).reverse()
+
+const deepClone = (obj: unknown) => JSON.parse(JSON.stringify(obj))
+
+const equalityCompare = (
+	a: { [key: string]: any },
+	b: { [key: string]: any }
+): boolean => JSON.stringify(a) === JSON.stringify(b)
 
 const spawnVal = () => (Math.random() <= 0.9 ? 2 : 4)
+
+const spawn = (arr: CellsType) => {
+	let index
+	do {
+		index = Math.floor(Math.random() * 16)
+	} while (arr[index].val !== 0)
+	arr[index].val = spawnVal()
+	return arr
+}
 
 const initializeGame = () => {
 	let arr = Array(16).fill(null)
@@ -186,144 +248,45 @@ const initializeGame = () => {
 		}
 		arr[i] = cell
 	}
-	arr = cellsZ.parse(arr)
+	arr = CellsZod.parse(arr)
 
-	let index, index2
-	do {
-		index = Math.floor(Math.random() * 16)
-		index2 = Math.floor(Math.random() * 16)
-	} while (index2 == index)
-	arr[index].val = spawnVal()
-	arr[index2].val = spawnVal()
+	arr = spawn(arr)
+	arr = spawn(arr)
 
-	return arr as cellsType
+	return arr as CellsType
 }
 
-// Expecting all line move to the left
-const moveOrMerge = (arr: cellType[]) => {
-	// Deep clone
-	let tmp = (JSON.parse(JSON.stringify(arr)) as cellType[])
-		.map((cell, index) => ({
-			cell,
-			index
-		}))
-		.filter(item => item.cell.val != 0)
-	let actions = []
-	if (tmp[0] && tmp[0].index != 0) {
-		actions.push({
-			type: "move",
-			payload: {
-				cellID: tmp[0].cell.id,
-				from: tmp[0].cell.cor,
-				to: arr[0].cor,
-				unmountCellID: arr[0].id
+const CordinateZod = z.object({
+	row: z.number().max(3),
+	col: z.number().max(3)
+})
+
+const cellZ = z.object({
+	id: z.string(),
+	val: z.number().default(0),
+	cor: CordinateZod,
+	prevCor: CordinateZod.optional()
+})
+
+const CellsZod = z.array(cellZ)
+type CellsType = z.infer<typeof CellsZod>
+
+const useGlobalKeyDownEffect = <T extends unknown>(
+	keys: string[],
+	callbacks: ((deps: T[]) => void)[],
+	dependencies: T[]
+) => {
+	const keyDownCallback = (e: KeyboardEvent) => {
+		keys.forEach((key, index) => {
+			if (e.key == key) {
+				callbacks[index](dependencies)
 			}
 		})
-		tmp[0].cell.cor = arr[0].cor
 	}
-	let prev = tmp[0]
-	for (let i = 1; i < tmp.length; i++) {
-		let curr = tmp[i]
-
-		if (prev.cell.val === curr.cell.val) {
-			actions.push({
-				type: "merge",
-				payload: {
-					cellID: curr.cell.id,
-					from: curr.cell.cor,
-					to: prev.cell.cor,
-					unmountCellID: prev.cell.id
-				}
-			})
-			curr.cell.cor = prev.cell.cor
-		} else {
-			let cr = curr.cell.cor.row
-			let cc = curr.cell.cor.col
-			let pr = prev.cell.cor.row
-			let pc = prev.cell.cor.col
-			let to =
-				cr == pr
-					? { row: pr, col: pc + (cc - pc > 0 ? 1 : -1) }
-					: { col: pc, row: pr + (cr - pr > 0 ? 1 : -1) }
-			const old = arr.find(
-				item => item.cor.row == to.row && item.cor.col == to.col
-			)!
-			if (curr.cell.cor != to) {
-				actions.push({
-					type: "move",
-					payload: {
-						cellID: curr.cell.id,
-						from: curr.cell.cor,
-						to: to,
-						unmountCellID: old.id
-					}
-				})
-				curr.cell.cor = to
-			}
+	useEffect(() => {
+		window.addEventListener("keydown", keyDownCallback)
+		return () => {
+			window.removeEventListener("keydown", keyDownCallback)
 		}
-
-		prev = curr
-	}
-	return actions
+	}, [dependencies])
 }
-
-const merge = (from: cellType, to: cellType) => {
-	move(from, to)
-	to.val *= 2
-}
-
-const move = (from: cellType, to: cellType) => {
-	to.val = from.val
-	from.val = 0
-}
-
-const leftSequence = (i: number) => [i * 4, i * 4 + 1, i * 4 + 2, i * 4 + 3]
-const rightSequence = (i: number) => leftSequence(i).reverse()
-const upSequence = (i: number) => [i, i + 4, i + 8, i + 12]
-const downSequence = (i: number) => upSequence(i).reverse()
-
-const indexToRowCol = (i: number) => {
-	const row = Math.floor(i / 4)
-	const col = i % 4
-	return { row, col }
-}
-
-const onArrowKeyDown = (
-	sequence: (i: number) => number[],
-	dispatch: React.Dispatch<ActionsType>
-) => {
-	return (deps: unknown[]) => {
-		const [cells, endState] = deps as [
-			cellsType,
-			[boolean, React.Dispatch<React.SetStateAction<boolean>>]
-		]
-
-		const [, setIsEnd] = endState as [
-			boolean,
-			React.Dispatch<React.SetStateAction<boolean>>
-		]
-
-		let arr = [0, 1, 2, 3]
-			.map(i =>
-				sequence(i)
-					.map(indexToRowCol)
-					.map(
-						({ row, col }) =>
-							cells.find(cell => cell.cor.row === row && cell.cor.col === col)!
-					)
-			)
-			.map(arr => moveOrMerge(arr))
-			.flat()
-
-		dispatch({ type: "batch", payload: { actions: arr as ActionType[] } })
-	}
-}
-
-const onArrowLeftKeyDown = (dispatch: React.Dispatch<ActionsType>) =>
-	onArrowKeyDown(leftSequence, dispatch)
-const onArrowRightKeyDown = (dispatch: React.Dispatch<ActionsType>) =>
-	onArrowKeyDown(rightSequence, dispatch)
-const onArrowUpKeyDown = (dispatch: React.Dispatch<ActionsType>) =>
-	onArrowKeyDown(upSequence, dispatch)
-const onArrowDownKeyDown = (dispatch: React.Dispatch<ActionsType>) =>
-	onArrowKeyDown(downSequence, dispatch)
